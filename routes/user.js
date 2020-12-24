d5a2ae56da251ebd9ad2644147d4cde97d0ebf0a
CustomizedMe <prakharbhatnagar5337@gmail.com>
2020-12-25 00:19:45 +0530

    Routes updated

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-//@route GET api/users
-//@desc Test route
-//@access public
-router.get('/', (req,res) => req.setEncoding('User route'));
-
-module.exports = router; 
