4f177a740fb8fe383af3683ed1793cb7643d7d87
Pranav Gupta <pranavgupta194@gmail.com>
2021-01-16 23:51:02 +0530

    minor change

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-const mongoose =  require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+require("dotenv").config();
+const db = process.env.mongoURI;
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(db,{
-            useNewUrlParser : true,
-            useUnifiedTopology: true,
-            useCreateIndex : true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB Connected...');
-
-    } catch(err){
-        console.error(err.message);
-        //Exit
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    //Exit
+    process.exit(1);
+  }
+};
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./config/db");
+require("dotenv").config();
 
 connectDB();
 
