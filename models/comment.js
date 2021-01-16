6e952f8abee96e7a404259d71c410b6e6fc1dab0
Pranav Gupta <pranavgupta194@gmail.com>
2021-01-16 23:51:57 +0530

    minor changes

--- models/comment.js
+++ models/comment.js
@@ -1,48 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const CommentSchema = new Schema({
-  symbol :{
-      type : String, 
-      required: true,
+  symbol: {
+    type: String,
+    required: true,
   },
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'users'
+    ref: "users",
   },
   text: {
     type: String,
-    required: true
+    required: true,
   },
   username: {
-    type: String
+    type: String,
   },
-  comments: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      },
-      text: {
-        type: String,
-        required: true
-      },
-      username: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      }
-    }
-  ],
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-
-
-module.exports = Comment = mongoose.model('comment', CommentSchema);
+module.exports = Comment = mongoose.model("comment", CommentSchema);
