280c751ea10504a0f58536efb2c4892e815a5a11
Pranav Gupta <pranavgupta194@gmail.com>
2021-01-17 12:13:30 +0530

    cleanup

--- client/src/Store/index.js
+++ client/src/Store/index.js
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-
+import initialState from "../InitialState";
 import reducer from "../Reducer";
 //import setAuthToken from './utils/setAuthToken';
-
-const initialState = {};
 
 const middleware = [thunk];
 
